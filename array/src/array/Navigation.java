d49b01f02249de84807a97d56557ca5aab6ba98d
mohanic2 <mohanic2@gmail.com>
2022-09-12 00:25:33 +0530

    mahender commiu

---
+++ array/src/array/Navigation.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Navigation {
+
+	public static void main(String[] args) {
+
+		System.out.println("mahender commited1");
+		System.out.println("mahender commited2");
+		System.out.println("mahender commited2");
+		System.out.println("mahender commited2");
+	}
+
+}
